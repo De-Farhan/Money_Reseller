11a105411acc440babbf57eaaa96a269643b4edf
fayzur <fayzurrahman991@gmail.com>
2021-02-24 21:58:03 +0600

    first commit

---
+++ js/main_script.js
@@ -0,0 +1,20 @@
+
+/*-----------vano box js start------------*/
+$(document).ready(function(){
+    $('#main_content .content_wrapper .venobox').venobox(); 
+  });
+/*-----------vano box js end------------*/
+
+/*-----------to bottom js start------------*/
+$(window).on("scroll",function(){
+  var scrollamount = $(window).scrollTop();
+  if(scrollamount >= 780){
+    $(".to_top").addClass("fixed_to_top");
+  }
+  else{
+    $(".to_top").removeClass("fixed_to_top");
+  }
+});
+/*-----------to bottom js end------------*/
+
+
